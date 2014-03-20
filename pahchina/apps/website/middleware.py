28d865e86ce3be8556ad5b09e1502425afbd7bd0
zhwei <sdutzhangwei@gmail.com>
2014-03-20 16:22:30 +0800

    ii

--- pahchina/apps/website/middleware.py
+++ pahchina/apps/website/middleware.py
@@ -15,17 +15,11 @@ class SubSiteMiddleWare:
     """
     def process_request(self, request):
         host = request.META["HTTP_HOST"].split(":")[0]
-        logging.info(host)
+        logging.error(host)
         try:
             request.SITE = Site.objects.get(domain=host)
         except Site.DoesNotExist:
             request.SITE = Site.objects.get(id=1)
-        #if host in ('127.0.0.1', '0.0.0.0', settings.DOMAIN):
-
-        #elif host in [d.domain for d in Site.objects.all()]:
-        #        request.urlconf = settings.SUBSITE_URLCONF
-        #else:
-        #    pass
 
     def process_response(self, request, response):
 
